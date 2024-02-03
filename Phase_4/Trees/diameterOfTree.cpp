fe8e02b1ae6e3a8dbfce350ec3bd76bee424416a
Puneethkumar <puneethkumarg96@gmail.com>
2024-02-03 12:39:33 +0530

    diameter of binary tree

--- Phase_4/Trees/diameterOfTree.cpp
+++ Phase_4/Trees/diameterOfTree.cpp
@@ -1,38 +1,64 @@
 #include<iostream>
 #include<queue>
-
 using namespace std;
 
 class Node{
     public:
     int data;
-    Node* right;
     Node* left;
+    Node* right;
 
     Node(int d){
         this->data = d;
-        this->left = NULL;
-        this->right = NULL;
+        left = NULL;
+        right = NULL;
     }
 };
 
 Node* buildTree(Node* &root){
-    cout<<"Enter the vaule of the node"<<endl;
+    cout<<"Enter the data of the root node"<< endl;
     int data;
     cin>>data;
     root = new Node(data);
-   
 
     if(data == -1){
         return NULL;
     }
 
-    cout<<"Enter the value for inserting left node of "<<root->data<<endl;
+    cout<<"Enter the value to insert into left of node -> "<<data<<endl;
     root->left = buildTree(root->left);
-    cout<<"Enter the value for inserting right node of "<<root->data<<endl;
+    cout<<"Enter the value to insert into right of node -> "<<data<<endl;
+    cout<<root->data<<endl;
     root->right = buildTree(root->right);
     return root;
+   // 2 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
 }
+
+
+int heightOfTree(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+
+    int leftHeight = heightOfTree(root->left);
+    int rightHeight = heightOfTree(root->right);
+
+    return max(leftHeight,rightHeight);    
+}
+
+int Diameter(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+
+    int op1 = Diameter(root->left);
+    int op2 = Diameter(root->right);
+    int op3 = heightOfTree(root->left)+heightOfTree(root->right)+1;
+
+    int ans = max(op1,max(op2,op3));
+    return ans;
+}
+
 
 void levelOrderTraversal(Node* root){
     queue<Node*> q;
@@ -42,7 +68,7 @@ void levelOrderTraversal(Node* root){
     while(!q.empty()){
         Node* temp = q.front();
         q.pop();
-
+        
         if(temp == NULL){
             cout<<endl;
             if(!q.empty()){
@@ -61,20 +87,12 @@ void levelOrderTraversal(Node* root){
     }
 }
 
-int heightOfTree(Node* root){
-    if(root == NULL){
-        return 0;
-    }
-
-    int leftHeight =  heightOfTree(root->left)+1;
-    int rightHeight = heightOfTree(root->right)+1;
-    return max(leftHeight,rightHeight);
-}
-
 int main(){
     Node* root;
 
     buildTree(root);
     levelOrderTraversal(root);
-    cout<<"Height of the tree is -> "<<heightOfTree(root)<<endl;
+
+    cout<<"Diameter of the Tree is -> "<<Diameter(root)<<endl;
+
 }
