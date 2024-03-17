9914565db7868ab3ad6c362d402bca6b3256f7e7
Puneethkumar <puneethkumarg96@gmail.com>
2024-03-17 08:51:06 +0530

    vertical line order traversal

---
+++ Phase_4/Trees/Tree traversals/verticalLineOrder.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <queue>
+#include <map>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+Node *buildtree(Node *&root)
+{
+    cout << "Enter the data of the node" << endl;
+    int data;
+    cin >> data;
+    if (data == -1)
+    {
+        return NULL;
+    }
+    root = new Node(data);
+
+    cout << "Enter the left node of " << data << " ?" << endl;
+    root->left = buildtree(root->left);
+    cout << "Enter the right node of " << data << " ?" << endl;
+    root->right = buildtree(root->right);
+
+    return root;
+}
+
+void printTree(Node *&root)
+{
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+
+        if (temp == NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << " ";
+
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+vector<int> verticalLineOrderTraversal(Node* &root){
+    //first is HD(horizontal dist.) next is Level
+    map<int,map<int,vector<int>>> nodes;
+
+    //pair int int is for horizontal distance and level
+    queue<pair<Node*,pair<int,int>>> q;
+    vector<int> ans;
+    if(root == NULL){
+        return ans;
+    }
+
+    q.push(make_pair(root,make_pair(0,0)));
+
+    while (!q.empty())
+    {
+        pair<Node *, pair<int, int>> temp = q.front();
+        q.pop();
+
+        Node* frontNode = temp.first;
+        int hd = temp.second.first;
+        int lvl = temp.second.second;
+
+        nodes[hd][lvl].push_back(frontNode->data);
+
+        if(frontNode->left){
+            q.push(make_pair(root->left, make_pair(hd-1, lvl+1)));
+        }
+        if(frontNode->right){
+            q.push(make_pair(frontNode->right,make_pair(hd+1,lvl+1)));
+        }
+
+    }
+
+    for(auto i:nodes){
+        for(auto j:i.second){
+            for(auto k:j.second){
+                ans.push_back(k);
+            }
+        }
+    }
+    return ans;
+    
+}
+
+int main(){
+    Node* root = buildtree(root);
+    vector<int> result = verticalLineOrderTraversal(root);
+
+    printTree(root);
+
+    cout<<"Vertical line order traversal :"<<endl;
+    
+    cout<<result.size()<<endl;
+    
+    cout<<endl;
+    return 0;
+}
+// 1 2 4 -1 -1 5 -1 -1 3 6 -1 8 -1 -1 7 -1 9 -1 -1
