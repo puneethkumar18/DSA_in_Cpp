93357bc0eff6021a688a518f8c09693844122388
Puneethkumar <puneethkumarg96@gmail.com>
2024-03-29 11:58:36 +0530

    kth smallest element of the BST

---
+++ Phase_4/Trees/BinarySearchTree/findKthSmallestElement.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+void insertNode(Node *&root, int data)
+{
+    if (root == NULL)
+    {
+        Node *temp = new Node(data);
+        root = temp;
+        return;
+    }
+    if (root->data < data)
+    {
+        insertNode(root->right, data);
+    }
+    else
+    {
+        insertNode(root->left, data);
+    }
+}
+void inOrderPrint(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inOrderPrint(root->left);
+    cout << root->data << " ";
+    inOrderPrint(root->right);
+}
+//kth element means if k = 3 3rd element of the inOrderTravesel
+int kthSmallestElement(Node* root, int &i,int k){
+    if(root == NULL){
+        return -1;
+    }
+    int left = kthSmallestElement(root->left,i,k);
+    if(left != -1){
+        return left;
+    }
+    i++;
+    if(i == k){
+        return root->data;
+    }
+    return kthSmallestElement(root->right,i,k);
+} 
+int main()
+{
+    vector<int> list = {10, 7, 5, 8, 15, 17, 16, 20};
+    int size = list.size();
+    Node *root = NULL;
+    for (int i = 0; i < size; i++)
+    {
+        insertNode(root, list[i]);
+    }
+    int k;
+    int i = 0;
+    cout<<"InOrder ->"<<endl;
+    inOrderPrint(root);
+    cout<<endl;
+    cout<< "enter the k value -> ";
+    cin>>k;
+    cout<<endl;
+    cout<<"Kth smallest elemet of the tree is ->"<<kthSmallestElement(root,i,k)<<endl;
+    return 0;
+}
