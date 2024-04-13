a178fbfa122b9e69161dbe8bde8cccaaa0ad283a
Puneethkumar <puneethkumarg96@gmail.com>
2024-04-13 20:41:04 +0530

    minCostOfRopes2

--- Phase_4/Heaps/heap_problems/minCostOfRopes.cpp
+++ Phase_4/Heaps/heap_problems/minCostOfRopes.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<queue>
+
 using namespace std;
 
 int minCostOfRopes(vector<int> arr,int n){
@@ -12,11 +14,37 @@ int minCostOfRopes(vector<int> arr,int n){
         arr[i] = arr[i+1];
     }
     int res = minCostOfRopes(arr,n-1);
-
     return ans+res;
+}
+
+//approch 2
+int minCostOfRopes2(vector<int> arr){
+    priority_queue<int,vector<int>, greater<int>> pq;
+
+    for(int i = 0;i<arr.size();i++){
+        pq.push(arr[i]);
+    }
+
+    int cost = 0;
+
+    while (pq.size() > 1)
+    {
+        int a = pq.top();
+        pq.pop();
+        int b = pq.top();
+        pq.pop();
+        int sum = a+b;
+
+        cost += sum;
+
+        pq.push(sum);
+    }
+    
+    return cost;
 }
 int main(){
     vector<int> heap = {4,3,2,6};
     cout<<"The Result of the problem is -> "<<minCostOfRopes(heap,4)<<endl;
+    cout << "The Result of the problem is by Using Priority Queue -> " << minCostOfRopes2(heap) << endl;
     return 0;
 }
