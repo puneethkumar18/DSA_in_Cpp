1baa3594425dcc35621330e627023b1aca3605d7
Puneethkumar <puneethkumarg96@gmail.com>
2024-04-13 11:30:09 +0530

    heapsort

--- Phase_4/Heaps/heapImplementation.cpp
+++ Phase_4/Heaps/heapImplementation.cpp
@@ -77,12 +77,12 @@ void heapify(int arr[], int n, int i)
     int left = 2 * i;
     int right = 2 * i + 1;
 
-    if (left < n && arr[largest] < arr[left])
+    if (left <= n && arr[largest] < arr[left])
     {
         largest = left;
     }
 
-    if (right < n && arr[largest] < arr[right])
+    if (right <= n && arr[largest] < arr[right])
         largest = right;
 
     if (largest != i)
@@ -90,6 +90,17 @@ void heapify(int arr[], int n, int i)
         swap(arr[largest], arr[i]);
         heapify(arr, n, largest);
     }
+}
+
+void heapSort(int arr[],int n){
+    int size = n;
+    while (size > 1)
+    {
+        swap(arr[size],arr[1]);
+        size--;
+
+        heapify(arr,size,1);
+    } 
 }
 
 int main(){
@@ -115,9 +126,21 @@ int main(){
 
     cout<<"Printing the Array new "<<endl;
 
-    for(int i=1;i<n;i++){
+    for(int i=1;i <= n;i++){
         cout<<arr[i]<<" ";
     }
     cout<<endl;
+
+    heapSort(arr,n);
+    cout << "Printing the HeapSort " << endl;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+
+    cout<<
     return 0;
 }
